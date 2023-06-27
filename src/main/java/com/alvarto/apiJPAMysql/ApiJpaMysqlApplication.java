6defe76d839d29480ec78d370e86e3f23eefd378
afloresg2 <afloresg2@fcpn.edu.bo>
2023-06-27 12:08:48 -0400

    first commit

---
+++ src/main/java/com/alvarto/apiJPAMysql/ApiJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.alvarto.apiJPAMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJpaMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvarto/apiJPAMysql/ApiJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarto.apiJPAMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ApiJpaMysqlApplicationTests.class)
+class ApiJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
